8d393aae99b872fd918ed10c592171abcca31103
UO277646 <UO277646@uniovi.es>
2022-02-14 11:27:26 +0100

    Initial commit

---
+++ src/test/java/com/uniovi/sdi2122102spring/Sdi2122102SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122102spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122102SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
